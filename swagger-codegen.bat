79c87493992fd234ed127c46d9d4cf46d006256d
quannm <quannm@songphi.com>
2018-09-13 15:48:50 +0700

    change param put forgot password

---
+++ swagger-codegen.bat
@@ -0,0 +1,9 @@
+rem author: https://github.com/marcelothebuilder
+rem date: 2017-03-06
+
+rem You need to put this file together with swagger-codegen-cli jar inside any folder.
+rem Then, add that folder to the PATH system variable to make the tool available anywhere.
+rem You can find the .jar link here: https://github.com/swagger-api/swagger-codegen#prerequisites
+
+@echo off
+java -jar D:\WS\swagger\swagger-codegen-cli.jar %*
